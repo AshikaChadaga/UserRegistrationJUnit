57df323c2703e0be014803cec5d1f1f9bcd58a48
AshikaChadaga <ashikachadaga@gmail.com>
2021-09-08 15:44:44 +0530

    ADDED: method to validate email

--- src/main/java/com/bridgelabz/userregistrationjunit/UserValidation.java
+++ src/main/java/com/bridgelabz/userregistrationjunit/UserValidation.java
@@ -7,5 +7,10 @@ public class UserValidation {
 		String nameRegex = "^[A-Z]{1}[a-z]{2,}$";
 		return name.matches(nameRegex);
 	}
+	public boolean validateEmail(String email) {
+
+		String emailRegex = "^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$";
+		return email.matches(emailRegex);
+	}
 
 }
