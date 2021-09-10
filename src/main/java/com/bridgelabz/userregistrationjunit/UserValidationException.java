c0a150c982f6a7f8fe8da851fb159cefdf58511e
AshikaChadaga <ashikachadaga@gmail.com>
2021-09-10 13:43:08 +0530

    ADDED: Custom Exception class

---
+++ src/main/java/com/bridgelabz/userregistrationjunit/UserValidationException.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.userregistrationjunit;
+
+
+public class UserValidationException extends RuntimeException{
+
+	public UserValidationException(String message) {
+
+		super(message);
+		
+	}
+}
